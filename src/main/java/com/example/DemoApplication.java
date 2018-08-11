6e3fcdbedc714f9bc382d05b0cd8954d1162100b
anil <anil@office-mini.hsd1.ca.comcast.net>
2018-08-10 17:15:11 -0700

    ss

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -16,6 +16,7 @@ public class DemoApplication {
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
+		//sam
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
